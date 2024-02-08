465904dca749d5c9f00b1e61029b19025705c06c
abdellah khalid <37934938+khalidabdou@users.noreply.github.com>
2024-02-08 14:41:32 +0100

    next vesion of materail 3

--- app/src/main/java/com/wishes/jetpackcompose/screens/Splash.kt
+++ app/src/main/java/com/wishes/jetpackcompose/screens/Splash.kt
@@ -1,15 +1,16 @@
 package com.wishes.jetpackcompose.screens
 
 import android.util.Log
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -21,16 +22,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.wishes.jetpackcompose.R
-import com.wishes.jetpackcompose.runtime.NavRoutes
 import com.wishes.jetpackcompose.utlis.Resource
 import com.wishes.jetpackcompose.viewModel.ImagesViewModel
 import kotlinx.coroutines.delay
@@ -62,7 +62,7 @@ fun Splash(navController: NavHostController, viewModel: ImagesViewModel) {
         }
 
         is Resource.Success -> {
-            navController.navigate(NavRoutes.Home.route)
+            //navController.navigate(NavRoutes.Home.route)
         }
 
         else -> {
@@ -71,7 +71,7 @@ fun Splash(navController: NavHostController, viewModel: ImagesViewModel) {
                 startAnimation = true
                 delay(2000)
                 navController.popBackStack()
-                navController.navigate(NavRoutes.Home.route)
+                //navController.navigate(NavRoutes.Home.route)
                 //Toast.makeText(context, "${viewModel.adsList.value}", Toast.LENGTH_LONG).show()
             }
         }
@@ -83,23 +83,18 @@ fun Splash(navController: NavHostController, viewModel: ImagesViewModel) {
 
 @Composable
 fun Splash(alpha: Float, message: String) {
+
+    val images: List<String> = listOf("", "", "")
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
-        Image(
-            modifier = Modifier
-                .size(120.dp)
-                .clip(CircleShape),
-            painter = painterResource(id = R.mipmap.ic_launcher_foreground),
-            contentDescription = "Logo Icon",
 
-            //tint = Color.DarkGray
-        )
+        AnimateImageScaleAndRotate()
         Text(
             text = stringResource(id = R.string.app_name),
             color = MaterialTheme.colorScheme.onBackground,
@@ -111,4 +106,35 @@ fun Splash(alpha: Float, message: String) {
         )
     }
 }
+
+
+@Composable
+fun AnimateImageScaleAndRotate() {
+    var scaledUp by remember { mutableStateOf(false) }
+    val scale = animateFloatAsState(
+        targetValue = if (scaledUp) 3f else 1f,
+        animationSpec = tween(durationMillis = 2000), // 2 seconds duration
+        label = ""
+    )
+    val rotation = animateFloatAsState(
+        targetValue = if (scaledUp) 20f else 0f,
+        animationSpec = tween(durationMillis = 2000), // 2 seconds duration
+        label = ""
+    )
+
+    LaunchedEffect(key1 = Unit) {
+        scaledUp = true
+    }
+
+    Image(
+        painter = painterResource(id = R.drawable.image2),
+        contentDescription = "Animated Image",
+        modifier = Modifier.fillMaxWidth()
+            .scale(scale.value)
+            .rotate(rotation.value),
+
+    )
+
+}
+
 
